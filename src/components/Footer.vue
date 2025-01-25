bbcc91887a8cf1e37bb2fa6c7f6adbdfafeb6914
klayton Junior <klayton.junior@iaris.com>
2025-01-24 23:39:58 -0300

    feat: create Footer component following the figma specifications

---
+++ src/components/Footer.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+
+<template>
+  <footer class="w-full flex flex-col justify-center items-center py-8 px-10 bottom-0 z-10">
+    <h1 class="text-2xl text-white font-extrabold">Movie Now</h1>
+    <h3 class="text-md text-white">made with ❤️ <span class="font-bold">Klayton Jr</span></h3>
+  </footer>
+</template>
+
+<style></style>
